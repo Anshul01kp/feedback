563b1e9ba14b9b25456bdacfb58834cf0493a4f1
Anshul01kp <anshul01kp@gmail.com>
2018-04-06 01:08:47 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID:"150770087323-cq834jhaigu0lri7a3r77j1e7oa4f13i.apps.googleusercontent.com",
+  googleClientSecret:"4HF7SxGALbb6vH-PuYJL6YRO",
+  mongoURI: "mongodb://peabody:abc123@ds233739.mlab.com:33739/feedback-dev",
+  cookieKey: 'sdmnfbwkhfdacsmcasoudcsncadslcjbkadscnasocjnds'
+}
